5cca5451b243903c6b541bc94c4c27b80f49f800
tonaramos <gtonaramos@gmail.com>
2018-05-15 10:34:00 -0700

    Passing all test.

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,12 +1,5 @@
 var stubs = require('./spec/Stubs');
-/*************************************************************
-You should implement your request handler function in this file.
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-**************************************************************/
+
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -19,19 +12,15 @@ let message = {
 };
   
 var requestHandler = function(request, response) {
-  // console.log('Serving request type ' + request.method + ' for url ' + request.url);
   var statusCode = 200;
   var headers = defaultCorsHeaders;
   
   if (request.method === 'GET' && request.url === '/classes/messages') {
-
     if (request.url === '/favicon.ico') {
       response.writeHead(404, {'Content-Type': 'json'});
       response.end();
     } else {
       response.writeHead(200, {'Content-Type': 'json'});
-      // console.log('message results', message.results)
-      //response.write(JSON.stringify(message));
       response.end(JSON.stringify(message));
     }
   } else if (request.method === 'POST' && request.url === '/classes/messages') {
@@ -41,8 +30,7 @@ var requestHandler = function(request, response) {
     }).on('end', () => {     
       body = Buffer.concat(body).toString();
       message.results.push(JSON.parse(body));
-      response.statusCode = 201;
-      //console.log("Message.results -----------", message.results);
+      response.writeHead(201, {'Content-Type': 'json'});
       response.end();
     });
     
@@ -51,6 +39,5 @@ var requestHandler = function(request, response) {
     response.end();
   }
 };
-
 
 exports.requestHandler = requestHandler;
